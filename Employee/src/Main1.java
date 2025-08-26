e6c37f00fc43c6fb4ed29b93868c6dacf59be38b
Aryan singh <aryansingh2.0091@gmail.com>
2025-08-26 19:18:24 +0530

    Delete Employee directory

--- Employee/src/Main1.java
+++
@@ -1,7 +0,0 @@
-public class Main1 {
-    public static void main(String[] args) {
-        College_student1 cs = new College_student1("ghaziabad" , 89);
-        cs.show_roll();
-        cs.show_address();
-    }
-}
